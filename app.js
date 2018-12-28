efe6a03692c81fd60f27f2e9afc3a4201aae58d1
Adam Smith <adams@cipafilter.com>
2018-12-28 15:04:32 -0600

    000005: Add error page and players page

--- app.js
+++ app.js
@@ -8,6 +8,15 @@ app.get("/", (req, res) => {
 	res.render("pages/landing");
 });
 
+app.get("/players", (req, res) => {
+	const players = [
+		{name: "Arlandor", styles: ""},
+		{name: "DKP", styles: ""},
+	];
+
+	res.render("pages/players", {players:players});
+});
+
 app.listen(3000, () => {
 	console.log("Starting app...");
 });
